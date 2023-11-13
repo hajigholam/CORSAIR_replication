b7e75650272a03c7a0fbd5d078b69d18b1860629
Amirhossein Hajigholam Saryazdi <72343516+hajigholam@users.noreply.github.com>
2023-11-13 10:17:14 -0500

    Update _public.py

--- code/_public.py
+++ code/_public.py
@@ -38,7 +38,7 @@ Dataset_Names = ['Twitter']
 # ['HyperPartisan','Twitter','ARC','SCIERC','ChemProt','Economy','20News','Parties','Yelp_Hotel','Taobao','Suning']
 EDA = False
 Weight = False
-Base_Model = 'TextCNN'
+Base_Model = 'RoBERTa'
 # TextCNN RoBERTa
 Save_Path = './Results.txt'
 
